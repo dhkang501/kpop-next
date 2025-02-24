96fe215655a8202cd2eb03957e1e0a17be4036f7
dhkang501 <kangg5@naver.com>
2025-02-24 14:21:02 +0900

    -vercel 배포오류 수정중 - 오류 원인 : ./src/app/(site)/page.tsx Module not found: Can't resolve '@/lib/dummydata'

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,7 +3,7 @@ import { useRef, useEffect } from "react";
 import CategoryContent from "@/components/categoryContent/CategoryContent";
 import { useCategoryStore } from "@/store/useCategoryStore";
 import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
-import { categories } from "@/lib/dummydata";
+import { categories } from "../../lib/dummydata";
 
 export default function Home() {
   const { selectCategory, setCategory } = useCategoryStore();
